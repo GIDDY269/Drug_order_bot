ffb92c531dd9560629ea3c0caacf776dfbe8dc91
giddy <oviemunooboro@gmail.com>
2024-06-13 14:34:17 +0100

    'drug_bot'

--- order_automation.py
+++ order_automation.py
@@ -9,7 +9,7 @@ from selenium.common.exceptions import TimeoutException, NoSuchElementException,
 import time
 from io import BytesIO
 from PIL import Image
-import chromedriver_autoinstaller
+
 
 
 
@@ -51,11 +51,11 @@ def order_automation(items:List):
     
     'To make order from web'
     url = 'https://glovoapp.com/ng/en/lagos/medplus-pharmacy-los/'
-    chromedriver_autoinstaller.install()
     chrome_options = Options()
     chrome_options.add_argument('--headless')
     chrome_options.add_argument('--no-sandbox')
     chrome_options.add_argument('--disable-dev-shm-usage')
+    chrome_options.add_argument("--disable-gpu")
     #chrome_options.chromedriver_executable = "chromedriver-win64/chromedriver.exe"
     try:
         driver = Chrome(options=chrome_options)
