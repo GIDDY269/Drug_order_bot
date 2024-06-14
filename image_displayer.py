e33e71da33406330432cebb23d60e4fcaaffe3b4
giddy <oviemunooboro@gmail.com>
2024-06-14 17:40:03 +0100

    'drug_bot'

--- image_displayer.py
+++ image_displayer.py
@@ -17,27 +17,25 @@ def extract_image_from_database(drug_name:str):
     #cnxn,cursor = create_sql_connection()
 
 
-    try:
-        data = pd.read_csv('Data_Ingestion\data2.csv')
-        sub_data = data[data['Product']==drug_name]
-        image_url = sub_data['Image_URL'].iloc[0]
+
+    data = pd.read_csv('Data_Ingestion\data2.csv')
+    sub_data = data[data['Product']==drug_name]
+    image_url = sub_data['Image_URL'].iloc[0]
         
         #select_query = 'SELECT image FROM DRUG_ORDER_CHATBOT1 WHERE name = ?'
         #cursor.execute(select_query, (drug_name,))
         # Fetch the result
         #result = cursor.fetchone()
 
-        try:
-            get_image = requests.get(image_url)
-            image = BytesIO(get_image.content)
+    try:
+        get_image = requests.get(image_url)
+        image = BytesIO(get_image.content)
             
-            return {f'This is what the image of the drug {drug_name} looks like':image}
+        return {f'This is what the image of the drug {drug_name} looks like':image}
             
-        except requests.exceptions.RequestException as e:
-            return {f"Error retrieving image: {e}": 'message'}     
-        
-    except Exception as e :
-        return {'Can not retrieve image' : 'message'}
+    except requests.exceptions.RequestException as e:
+        return {f"Error retrieving image: {e}": 'message'}     
+   
         
 
 
