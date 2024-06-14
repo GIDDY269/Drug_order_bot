7e2a43593d2ddc70df3c5cd42c759387252c63ea
giddy <oviemunooboro@gmail.com>
2024-06-14 16:51:12 +0100

    'drug_bot'

--- image_displayer.py
+++ image_displayer.py
@@ -5,6 +5,7 @@ import requests
 from io import BytesIO
 from langchain.agents import tool
 import pyodbc
+import pandas as pd
 
 
 
@@ -18,14 +19,17 @@ def extract_image_from_database(drug_name:str):
 
 
     try:
-        select_query = 'SELECT image FROM DRUG_ORDER_CHATBOT1 WHERE name = ?'
-        cursor.execute(select_query, (drug_name,))
+        data = pd.read_csv('Data_Ingestion\data2.csv')
+        sub_data = data[data['Product']==drug_name]
+        image_url = sub_data['Image_URL']
+        #select_query = 'SELECT image FROM DRUG_ORDER_CHATBOT1 WHERE name = ?'
+        #cursor.execute(select_query, (drug_name,))
         # Fetch the result
-        result = cursor.fetchone()
+        #result = cursor.fetchone()
     
-        if result:
+        if image_url:
             try:
-                get_image = requests.get(result[0])
+                get_image = requests.get(image_url)
             
                 image = BytesIO(get_image.content)
                 return {f'This is what the image of the drug {drug_name} looks like':image}
