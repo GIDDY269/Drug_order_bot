8e3999aa8e613a144cf14453e30fc6a1c4d54514
giddy <oviemunooboro@gmail.com>
2024-06-19 15:19:48 +0100

    Image diplay function

--- image_displayer.py
+++ image_displayer.py
@@ -1,31 +1,18 @@
-#from Data_Ingestion.SQL_Database import create_sql_connection
-#from Data_Ingestion.schema import CreateScrapeSchema
-from PIL import Image
+
 import requests
 from io import BytesIO
-
 import pandas as pd
-
 
 
 
 def extract_image_from_database(drug_name:str):
 
-    # create an sql connection
     'To display images'
-
-    #cnxn,cursor = create_sql_connection()
-
-
-
+    
     data = pd.read_csv('Data_Ingestion/data2.csv')
     sub_data = data[data['Product']==drug_name]
     image_url = sub_data['Image_URL'].iloc[0]
         
-        #select_query = 'SELECT image FROM DRUG_ORDER_CHATBOT1 WHERE name = ?'
-        #cursor.execute(select_query, (drug_name,))
-        # Fetch the result
-        #result = cursor.fetchone()
 
     try:
         get_image = requests.get(image_url)
